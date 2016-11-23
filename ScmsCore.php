551a3bef04aa0152858a4fc08091e5f4ddf6209b
huwei <wei.hu@haoqitech.com.cn>
2016-11-23 09:57:21 +0800

    测试一下

--- ScmsCore.php
+++ ScmsCore.php
@@ -12,42 +12,18 @@ Trait ScmsCore
 {
     /* string 定义默认表名
      * */
-    private $scms_table = 'scms_content_list';
+    private $scms_table = 'testcomposer';
 
     /*string 定义默认字段名
      * */
     private $scms_fields = [
         'id'=>'id',
-        'category_id'=>'category_id',
-        'title'=>'title',
-        'summary'=>'summary',
-        'preview_big_image'=>'preview_big_image',
-        'preview_image'=>'preview_image',
-        'original_link'=>'original_link',
-        'published_time'=>'published_time',
-        'created_at'=>'created_at',
-        'modified_at'=>'modified_at',
-        'deleted_at'=>'deleted_at'
+        'title'=>'title'
     ];
     
     /*定义添加数据抽象方法
      * */
-    abstract public function addItem($datas);
-    /*定义删除数据抽象方法
-     * */
-    abstract public function deleteItem($id);
-    /*定义修改数据抽象方法
-     * */
-    abstract public function modifyItem($id,$datas);
-    /*定义根据id获取详细记录抽象方法
-     * */
-    abstract public function getItem($id);
-    /*定义获取记录列表抽象方法
-     * */
-    abstract public function getItems($datas);
-    /*定义恢复被删除数据抽象方法
-     * */
-    abstract public function resetItem($id);
+    abstract public function insert($datas);
 
     /*自定义设置表名
      * */
@@ -64,4 +40,8 @@ Trait ScmsCore
 
     /*一条一条的设置 数据库字段名？？？？？
      * */
+     public function addItem($data)
+    {
+        return $this->insert($data);
+    }
 }
